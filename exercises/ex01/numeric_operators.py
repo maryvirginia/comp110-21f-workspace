f4251ec8b5faad578168b330005c666405be1078
Mary Virginia Glennon <maryva@live.unc.edu>
2021-08-29 20:48:18 -0400

    Progess on EX01 numeric operators

---
+++ exercises/ex01/numeric_operators.py
@@ -0,0 +1,8 @@
+"""EX01 practicing numeric operators and type conversions"""
+
+__author__: str = "730224009"
+
+left_side: int = int(input("Left-hand side: "))
+right_side: int = int(input("Right-hand side: "))
+
+print(str(left_side) + " ** " + str(right_side) + " is " + str(left_side**right_side))
