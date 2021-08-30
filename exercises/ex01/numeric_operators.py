c0c4d306528cb17f7e9370552640c7e3cfcdee61
Mary Virginia Glennon <maryva@live.unc.edu>
2021-08-30 17:03:35 -0400

    Fixed Lint EX01 relational and numeric operators

--- exercises/ex01/numeric_operators.py
+++ exercises/ex01/numeric_operators.py
@@ -1,11 +1,11 @@
-"""EX01 practicing numeric operators and type conversions"""
+"""EX01 practicing numeric operators and type conversions."""
 
 __author__: str = "730224009"
 
 left_side: int = int(input("Left-hand side: "))
 right_side: int = int(input("Right-hand side: "))
 
-print(str(left_side) + " ** " + str(right_side) + " is " + str(left_side**right_side))
-print(str(left_side) + " / " + str(right_side) + " is " + str(left_side/right_side))
-print(str(left_side) + " // " + str(right_side) + " is " + str(left_side//right_side))
-print(str(left_side) + " % " + str(right_side) + " is " + str(left_side%right_side))
+print(str(left_side) + " ** " + str(right_side) + " is " + str(left_side ** right_side))
+print(str(left_side) + " / " + str(right_side) + " is " + str(left_side / right_side))
+print(str(left_side) + " // " + str(right_side) + " is " + str(left_side // right_side))
+print(str(left_side) + " % " + str(right_side) + " is " + str(left_side % right_side))
--- exercises/ex01/relational_operators.py
+++ exercises/ex01/relational_operators.py
@@ -1,11 +1,11 @@
-"""EX01 practicing relational operators"""
+"""EX01 practicing relational operators."""
 
 __author__: str = "730224009"
 
 left_side: int = int(input("Left-hand side: "))
 right_side: int = int(input("Right-hand side: "))
 
-print(str(left_side) + " < " + str(right_side) + " is " + str(left_side<right_side))
-print(str(left_side) + " >= " + str(right_side) + " is " + str(left_side>=right_side))
-print(str(left_side) + " == " +str(right_side) + " is " + str(left_side==right_side))
-print(str(left_side) + " != " +str(right_side) + " is " + str(left_side!=right_side))
+print(str(left_side) + " < " + str(right_side) + " is " + str(left_side < right_side))
+print(str(left_side) + " >= " + str(right_side) + " is " + str(left_side >= right_side))
+print(str(left_side) + " == " +str(right_side) + " is " + str(left_side == right_side))
+print(str(left_side) + " != " +str(right_side) + " is " + str(left_side != right_side))
