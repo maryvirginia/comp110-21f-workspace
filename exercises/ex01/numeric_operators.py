9478a66b35b2c6fdd9b8730b6847724e00508c26
Mary Virginia Glennon <maryva@live.unc.edu>
2021-08-30 16:37:14 -0400

    Finished EX01 numeric operators

--- exercises/ex01/numeric_operators.py
+++ exercises/ex01/numeric_operators.py
@@ -6,3 +6,6 @@ left_side: int = int(input("Left-hand side: "))
 right_side: int = int(input("Right-hand side: "))
 
 print(str(left_side) + " ** " + str(right_side) + " is " + str(left_side**right_side))
+print(str(left_side) + " / " + str(right_side) + " is " + str(left_side/right_side))
+print(str(left_side) + " // " + str(right_side) + " is " + str(left_side//right_side))
+print(str(left_side) + " % " + str(right_side) + " is " + str(left_side%right_side))
