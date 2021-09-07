48110d86208cf16838cfbf2ffb31bd5141d73a6f
Mary Virginia Glennon <maryva@live.unc.edu>
2021-09-07 16:49:56 -0400

    Finished EX02 count letters

--- exercises/ex02/count_letters.py
+++ exercises/ex02/count_letters.py
@@ -1,6 +1,19 @@
 """Counting letters in a string."""
 
-__author__ = "ENTER YOUR 9-DIGIT PID HERE"
+__author__ = "730224009"
 
 
 # Begin your solution here...
+given_letter: str = input("What letter do you want to search for?: ")
+given_word: str = input("Enter a word: ")
+counter: int = 0
+output_number: int = 0
+
+while counter < len(given_word):
+    if given_word[counter] == given_letter:
+        output_number = output_number + 1
+        counter = counter + 1
+    else:
+        counter = counter + 1
+
+print("Count: " + str(output_number))
