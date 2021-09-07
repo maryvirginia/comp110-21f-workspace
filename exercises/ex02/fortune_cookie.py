945100a1fb02671e680a99f91de90184186d5687
Mary Virginia Glennon <maryva@live.unc.edu>
2021-09-07 14:02:45 -0400

    Finished EX02 fortune cookie

--- exercises/ex02/fortune_cookie.py
+++ exercises/ex02/fortune_cookie.py
@@ -1,6 +1,6 @@
 """Program that outputs one of at least four random, good fortunes."""
 
-__author__ = "ENTER YOUR 9-DIGIT PID HERE"
+__author__ = "730224009"
 
 # The randint function is imported from the random library so that
 # you are able to generate integers at random.
@@ -13,3 +13,20 @@ from random import randint
 
 
 # Begin your solution here...
+print("Your fortune cookie says...")
+
+fortune: int = randint(1, 4)
+
+if fortune == 1:
+    print("You will face a challenge in the coming days, but your resilient spirit will prevail.")
+else:
+    if fortune == 2:
+        print("Your life will be filled with lots of love-- be sure to share it!")
+    else:
+        if fortune == 3:
+            print("You will meet your soulmate within the next two years!")
+        else:
+            if fortune == 4:
+                print("Continue to treat people with kindness, and good things will be coming your way!")
+
+print("Now, go spread positive vibes!")
