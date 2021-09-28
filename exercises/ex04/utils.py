e6acd74e87585bd452ef05076f8fdd32d0ca7727
Mary Virginia Glennon <maryva@live.unc.edu>
2021-09-28 09:13:03 -0400

    Work on EX04 all function

--- exercises/ex04/utils.py
+++ exercises/ex04/utils.py
@@ -1,6 +1,18 @@
 """List utility functions."""
 
-__author__ = "123456789"
+__author__ = "730224009"
 
 
 # TODO: Implement your functions here.
+
+
+def all(x: list[int], z: int) -> bool:
+    counter: int = 0
+    while counter < len(x):
+        item: int = x[counter]
+        if item != z:
+            return False
+        else:
+            counter += 1
+
+    return True
