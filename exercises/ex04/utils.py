5a02e6bf8da3db280d5d292fd0b58d30647ac039
Mary Virginia Glennon <maryva@live.unc.edu>
2021-09-28 20:09:02 -0400

    EX04 is_equal function

--- exercises/ex04/utils.py
+++ exercises/ex04/utils.py
@@ -16,3 +16,20 @@ def all(x: list[int], z: int) -> bool:
             counter += 1
 
     return True
+
+
+def is_equal(a: list[int], b: list[int]) -> bool:
+    counter_a: int = 0
+    counter_b: int = 0
+    if len(a) == len(b):
+        while counter_a < len(a):
+            item_a: int = a[counter_a]
+            item_b: int = b[counter_b]
+            if item_a == item_b:
+                counter_a += 1
+                counter_b += 1
+            else:
+                return False
+        return True
+    else:
+        return False
