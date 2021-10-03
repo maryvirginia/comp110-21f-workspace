fd6bda8d96be6864502e8e25ea4cfcf92dc57a2d
Mary Virginia Glennon <maryva@live.unc.edu>
2021-10-03 12:02:12 -0400

    work on EX05 only evens function

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -1,5 +1,20 @@
 """List utility functions part 2."""
 
-__author__ = "123456789"
+__author__ = "730224009"
 
 # Define your functions below
+
+
+def only_evens(a: list[int]) -> list[int]:
+    index: int = 0
+    item: int = a[index]
+    while index < len(a):
+        if item % 2 != 0:
+            a.pop(a[index])
+            index += 1
+        else:
+            index += 1
+    return a
+
+
+
