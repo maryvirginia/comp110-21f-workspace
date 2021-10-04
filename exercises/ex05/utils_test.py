84a55dad8cc07be9e1d9f46b194846d67af0e258
Mary Virginia Glennon <maryva@live.unc.edu>
2021-10-04 14:26:09 -0400

    finished EX05 unit tests

--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -1,6 +1,47 @@
 """Unit tests for list utility functions."""
 
 # TODO: Uncomment the below line when ready to write unit tests
-# from exercises.ex05.utils import only_evens, sub, concat
+from exercises.ex05.utils import only_evens, sub, concat
 
-__author__ = "123456789"
+__author__ = "730224009"
+
+
+# testing only_evens
+def test_only_evens_none() -> None:
+    assert only_evens([3, 5, 7]) == []
+
+
+def test_only_evens_empty() -> None:
+    assert only_evens([]) == []
+
+
+def test_only_evens_full() -> None:
+    assert only_evens([1, 2, 3, 4, 5, 6]) == [2, 4, 6]
+
+
+# testing sub
+def test_sub_neg_start() -> None:
+    assert sub([1, 2, 3], -3, 1) == [1]
+
+
+def test_sub_normal() -> None:
+    assert sub([3, 9, 6, 12], 1, 3) == [9, 6]
+
+
+def test_sub_small_end() -> None:
+    assert sub([0, 3, 8, 13, 44], 2, -3) == []
+
+
+# testing concat
+def test_concat_normal() -> None:
+    assert concat([1, 2, 3], [4, 5, 6]) == [1, 2, 3, 4, 5, 6]
+
+
+def test_concat_uno_empty() -> None:
+    assert concat([], [12, 33, 19]) == [12, 33, 19]
+
+
+def test_concat_both_empty() -> None:
+    assert concat([], []) == []
+
+
