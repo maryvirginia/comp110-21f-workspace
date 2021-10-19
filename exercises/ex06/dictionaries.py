5a1bb7f46793ce16716984c3a96b4db8faf8225b
Mary Virginia Glennon <maryva@live.unc.edu>
2021-10-18 21:10:44 -0400

    work on ex06 invert function

--- exercises/ex06/dictionaries.py
+++ exercises/ex06/dictionaries.py
@@ -6,3 +6,15 @@ __author__ = "730224009"
 
 
 def invert(a: dict[str, str]) -> dict[str, str]:
+    new_dict: dict[str, str] = {}
+    for key in a:
+        does_new_key_exist: bool = a[key] in new_dict
+        if does_new_key_exist == True:
+            # raise key error
+            raise KeyError("KeyError: new key already exists.")
+        else:
+            # proceed with adding key and value to new_dict
+            key = a[key] 
+            new_dict[key] = key
+
+    return new_dict
